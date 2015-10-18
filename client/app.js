2e99002263d22c2415f9fcb9e7bba7e0c8430492
RedWood <mackey91@gmail.com>
2015-10-18 00:21:58 -0700

    client side setup

---
+++ client/app.js
@@ -0,0 +1,23 @@
+angular.module('app',
+    [
+    'ui.router',
+    'icecomm.controller',
+    'icecomm.connect',
+    'icecomm.local',
+    'icecomm.peer',
+    'icecomm.leave',
+  ])
+  .config(function($httpProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
+    
+    $stateProvider
+        .state('chat', {
+            url: '/',
+            templateUrl: 'partials/chat.html',
+            controller: 'chatCtrl',
+            controllerAs: 'chatCtrl'
+        });
+       
+        $urlRouterProvider.otherwise('/login');
+        $locationProvider.html5Mode(true);
+
+  });
---
+++ client/controllers/chat.ctrl.js
@@ -0,0 +1,5 @@
+angular.module('app')
+  .controller('chatCtrl', ['$scope', function ($scope) {
+    
+
+  }]);
